6389ef070b79e9724e82785fd3b457d941d2eadf
Kashem Khondaker <kashem.khondaker.official001@gmail.com>
2025-09-01 12:13:58 +0600

    Home.jsx code refactor done .

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,5 @@
-import React from "react";
 import HomeHeroSection from "../../component/Home/HomeHeroSection";
 import Testimonials from "../../component/Home/Testimonials";
-import Features from "../../component/Home/Features";
-import PopularCourses from "../../component/Home/Popular-courses";
-// import CTA from "../../component/Home/CTA";
-import StatsSection from "../../component/Home/StatsSection";
 import BenefitsSection from "../../component/Home/BenefitsSection";
 import PricingSection from "../../component/Home/PricingSection";
 import FeaturedClasses from "../../component/Home/FeatuerdClasses";
@@ -17,29 +12,23 @@ const Home = () => {
       {/* Hero section */}
       <HomeHeroSection />
 
-      {/* Stats section */}
-      {/* <StatsSection /> */}
-
       {/* Benefits section */}
       <BenefitsSection />
 
       {/* Popular Courses section */}
-      {/* <PopularCourses /> */}
       <FeaturedClasses />
 
-      {/*Tastimonial section */}
+      {/*Testimonials section */}
       <Testimonials />
-      {/* Features section */}
-      {/* <Features /> */}
-      {/*  */}
 
       {/* Pricing section */}
       <PricingSection />
+
       {/* faq section  */}
       <FAQ />
+
       {/* CTA section */}
-      {/* <CTA /> */}
-      <CTA/>
+      <CTA />
     </div>
   );
 };
